3a9a39bd080b2155daa08611ce255c00c332c7fc
Nabilmuthi77 <Nabilmuthi77@gmail.com>
2022-12-15 17:36:01 -0500

    error euy

--- application/models/HomeModel.php
+++ application/models/HomeModel.php
@@ -3,5 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class HomeModel extends CI_Model
 {
-
+    public function pilih_kandidat()
+    {
+        $data = [
+            'id_user' => $this->input->post('id_user'),
+            'nama_kandidat' => $this->input->post('nama_kandidat'),
+        ];
+        $this->db->insert('suara', $data);
+    }
+    public function update_status_user()
+    {
+        $data = ['status' => 1];
+        $this->db->where('id_masyarakat', $this->input->post('id_user'));
+        $this->db->update('user', $data);
+    }
 }
