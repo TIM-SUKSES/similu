618c5f35fef50a864e33f377d491594db7097576
Alvin Austin <68804883+wandesay75@users.noreply.github.com>
2022-12-16 21:02:40 +0700

    alhamdulillah

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -31,7 +31,7 @@ class Home extends CI_Controller
     $this->load->view('visimisi', $data);
     $this->load->view('templates/footer', $data);
   }
-<<<<<<< HEAD
+
 
   public function pilih_kandidat()
   {
@@ -45,24 +45,5 @@ class Home extends CI_Controller
 
     echo json_encode($result);
   }
-
-  
-}
-=======
->>>>>>> 79c003ce44f39c152dbb94209723c263038e0b9b
-
-  public function pilih_kandidat()
-  {
-    $this->HomeModel->pilih_kandidat();
-    $this->HomeModel->update_status_user();
-    if ($this->db->affected_rows() > 0) {
-      $result = ['success' => true];
-    } else {
-      $result = ['success' => false];
-    }
-
-    echo json_encode($result);
-  }
-  
 
 }
